af05e3a197033e154c6dc0af19492a5330be18fe
Molto <y.ayoubwd@gmail.com>
2022-08-03 22:30:19 +0200

    record onChange input value local state

--- src/components/Section/CreateAlimentForm/CreateAlimentForm.js
+++ src/components/Section/CreateAlimentForm/CreateAlimentForm.js
@@ -1,24 +1,33 @@
+import { useState } from 'react'
 import { TextField } from '@material-ui/core'
+
 import { Button } from '@mui/material'
 import React from 'react'
 
 const CreateAlimentForm = () => {
-  
+  const [aliment, setAliment] = useState({})
+  console.log(aliment)
   return (
     <>
-    <label>Nom de l'aliment :
-      <input />
-    </label>
-    <label>Glucides :
-      <input />
-    </label>
-    <label>Proteines :
-      <input />
-    </label>
-    <label>Lipides :
-      <input />
-    </label>
+    <form>
+      <div className="form__input">
+        <label htmlFor='name'>Nom de l'aliment :</label>
+        <input id='name' onChange={(e) => setAliment({...aliment, [e.currentTarget.id]: e.currentTarget.value})}/>
+      </div>
+      <div className="form__input">
+        <label htmlFor='carbs'>Glucides : </label>
+        <input id='carbs' onChange={(e) => setAliment({...aliment, [e.currentTarget.id]: e.currentTarget.value})}/>
+      </div>
+      <div className="form__input">
+        <label htmlFor='protein'>Proteines :</label>
+        <input id='protein' onChange={(e) => setAliment({...aliment, [e.currentTarget.id]: e.currentTarget.value})}/>
+      </div>
+      <div className="form__input">
+        <label htmlFor='fat'>Lipides :</label>
+        <input id='fat' onChange={(e) => setAliment({...aliment, [e.currentTarget.id]: e.currentTarget.value})}/>
+      </div>
     <Button>Ajouter</Button>
+    </form>
     </>
   )
 }
